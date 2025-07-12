7b0e2908b105a6c24e6dd3ad3f038e35df651974
Geoffrey Mugford <65214363+NamidaSai@users.noreply.github.com>
2025-07-12 15:32:36 +0100

    fix exit triggering twice

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class Exit : MonoBehaviour
 {
     [SerializeField] private Color targetColor;
+
+    private bool _wasActivated = false;
     
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (_wasActivated) { return; }
         if (!other.CompareTag("Player")) { return; }
-        
+
+        _wasActivated = true;
         GameObject.FindWithTag("Player").GetComponent<PlayerAppearance>().SetAppearanceTo(targetColor);
         SceneLoader.Instance.LoadNextScene();
     }
