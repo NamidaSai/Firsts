217bea177b74e5c8baf8dfb4c0f9fa0b500590f0
Geoffrey Mugford <65214363+NamidaSai@users.noreply.github.com>
2025-08-26 16:18:52 +0100

    feat: add sfx code

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private Sound[] sounds = null;
+
+    public static AudioManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+
+        foreach (Sound sound in sounds)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+            sound.source.pitch = sound.pitch;
+            sound.source.loop = sound.loop;
+            sound.source.volume = sound.volume;
+        }
+    }
+
+    public void Play(string soundName)
+    {
+        Sound sound = Array.Find(sounds, soundClip => soundClip.name == soundName);
+
+        if (sound == null)
+        {
+            Debug.LogWarning("Sound: " + soundName + " not found.");
+            return;
+        }
+
+        if (sound.source == null) { return; }
+
+        if (sound.source.isPlaying) { return; }
+
+        if (sound.randomPitch)
+        {
+            sound.source.pitch = UnityEngine.Random.Range(sound.pitch - 0.2f, sound.pitch + 0.2f);
+        }
+        else
+        {
+            sound.source.pitch = sound.pitch;
+        }
+
+        sound.source.Play();
+    }
+}
---
+++ Assets/Scripts/AudioTrigger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour
+{
+    public void Trigger(string soundName)
+    {
+        if (string.IsNullOrEmpty(soundName)) { return; }
+        AudioManager.Instance.Play(soundName);
+    }
+}
