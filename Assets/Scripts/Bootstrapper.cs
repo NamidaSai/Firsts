3da0ecffd0bc4db920e68ae11ac0c5719d39bf06
Geoffrey Mugford <65214363+NamidaSai@users.noreply.github.com>
2025-11-23 09:51:10 +0000

    fix: try to stabilise browser cursor lock behaviour

---
+++ Assets/Scripts/Bootstrapper.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Bootstrapper : MonoBehaviour
+{
+    private void Awake()
+    {
+#if !UNITY_EDITOR && UNITY_WEBGL
+        WebGLInput.stickyCursorLock = false;
+        // setting stickyCursorLock to false keeps Cursor.lockState in sync with browser cursor lock state
+#endif
+    }
+}
